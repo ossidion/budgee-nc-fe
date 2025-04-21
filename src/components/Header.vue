06b8e1fe4e54d09143e25e651b2dd4d40d09c3e0
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-04-21 17:38:31 +0100

    adding theme

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,36 @@
 <script setup>
+import { ref, onMounted } from 'vue';
 
-// function toggleDark () {
-//   const element 
-// }
+const darkMode = ref(false);
 
+function toggleTheme() {
+  darkMode.value = !darkMode.value;
+  if (darkMode.value) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+  localStorage.setItem('darkMode', darkMode.value);
+}
 
+onMounted(() => {
+  const savedMode = localStorage.getItem('darkMode');
+  if (savedMode === 'true') {
+    darkMode.value = true;
+    document.body.classList.add('dark');
+  }
+});
 </script>
 
 <template>
+      <header class="text-2xl font-bold">Budgee</header>
 
-  <div class="header">
-    <header>Budgee</header>
-
-    <button @click="toggle-theme" class="header-button">
-      Toggle Mode
-    </button>
-  </div>
-
+      <button @click="toggleTheme"
+        class="header-button p-3 bg-gray-200 dark:bg-gray-700 text-black dark:text-white rounded-full hover:bg-gray-300 dark:hover:bg-gray-600 transition-all duration-300">
+        {{ darkMode ? 'Light Mode' : 'Dark Mode' }}
+      </button>
+  
 </template>
 
 <style scoped>
-
 </style>
