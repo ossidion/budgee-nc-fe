fb3d3db931b39637ac135ef0687db3052024d23c
PabloProgramming <pmontalvoster@gmail.com>
2025-04-22 14:00:07 +0100

    add left amount green when positive or red when negative

--- src/components/assets/stores/CustomChart.vue
+++ src/components/assets/stores/CustomChart.vue
@@ -1,9 +1,7 @@
-
 <script setup>
+import {useStore} from "./currentBudgetData";
 
-import { useStore } from './currentBudgetData';
-
-let budgetStore = useStore()
+let budgetStore = useStore();
 
 const props = defineProps({
   data: {
@@ -14,82 +12,71 @@ const props = defineProps({
       datasets: [
         {
           backgroundColor: [],
-          data: []
-        }
-      ]
+          data: [],
+        },
+      ],
     },
   },
   options: {
     type: Object,
     required: true,
-    default: {}
+    default: {},
   },
   periodRatio: {
     type: Number,
     required: true,
-    default: 0
-  }
+    default: 0,
+  },
 });
 
+import {Chart, DoughnutController} from "chart.js";
 
+import {createTypedChart} from "vue-chartjs";
 
-
-import { Chart, DoughnutController } from 'chart.js'
-
-
-
-import { createTypedChart } from 'vue-chartjs'
-
-
-
-class Custom extends DoughnutController{
+class Custom extends DoughnutController {
   updateElement(arc, i, properties, mode) {
-    
-
     // console.log(this.getDataset())
 
-    const outerRadius = properties.outerRadius
-    const innerRadius = properties.innerRadius
-    properties.innerRadius = innerRadius +this.getDataset().cutouts[i]|| 0
-    properties.outerRadius = outerRadius -this.getDataset().cutouts[i]|| 0
-
+    const outerRadius = properties.outerRadius;
+    const innerRadius = properties.innerRadius;
+    properties.innerRadius = innerRadius + this.getDataset().cutouts[i] || 0;
+    properties.outerRadius = outerRadius - this.getDataset().cutouts[i] || 0;
 
     // console.log(properties.innerRadius, new Date())
-    
-    super.updateElement(arc, i, properties, mode)
+
+    super.updateElement(arc, i, properties, mode);
   }
-  draw(){
-    super.draw(arguments)
+  draw() {
+    super.draw(arguments);
 
     // console.log(props.periodRatio)
     const ctx = this.chart.ctx;
   }
 }
-Custom.id = 'customDoughnut';
-Custom.defaults = DoughnutController.defaults
+Custom.id = "customDoughnut";
+Custom.defaults = DoughnutController.defaults;
 Chart.register(Custom);
 
-
-
-
-const CustomChart = createTypedChart("customDoughnut",Custom)
-
-
+const CustomChart = createTypedChart("customDoughnut", Custom);
 </script>
 
 <template>
+  <div class="donut-container">
+    <div class="donut-chart">
+      <CustomChart :data="data" :options="options"></CustomChart>
+    </div>
 
-
-<div class="donut-container">
-  <div class="donut-chart">
-    <CustomChart :data="data" :options="options"></CustomChart>  </div>
-
-  <div class="budget-overlay">
-    <p>£{{ budgetStore.budget.budget.toFixed(2) }}</p>
-    <p>left £{{ budgetStore.getSpendingLeft.toFixed(2) }}</p>
+    <div class="budget-overlay">
+      <p class="initial-budget">£{{ budgetStore.budget.budget.toFixed(2) }}</p>
+      <p
+        :class="[
+          'remaining-budget',
+          budgetStore.getSpendingLeft >= 0 ? 'positive' : 'negative',
+        ]">
+        left £{{ budgetStore.getSpendingLeft.toFixed(2) }}
+      </p>
+    </div>
   </div>
-</div>
-
 </template>
 
 <style scoped>
@@ -113,9 +100,22 @@ const CustomChart = createTypedChart("customDoughnut",Custom)
   text-align: center;
   z-index: 10;
   font-weight: bold;
-  font-size: 1rem;
-  pointer-events: none; 
+  font-size: 0.8rem;
+  pointer-events: none;
 }
 
+.initial-budget {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.25rem;
+}
+
+.remaining-budget.positive {
+  color: #73d622;
+}
+
+.remaining-budget.negative {
+  color: #dc2626;
+}
 </style>
 
