b06bdc4a9198bd5d9409a7b47bcd9922213e639c
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-04-23 11:20:58 +0100

    successfully deleted

--- src/components/CategoryCard.vue
+++ src/components/CategoryCard.vue
@@ -6,14 +6,7 @@ import { useCurrencyFormatter } from '@/utils/useCurrencyFormatter';
 import { deleteCategory }  from '@/api/requests'
 
 
-const toDeleteCategoryName = ref('')
-console.log(toDeleteCategoryName,"toDeleteCategoryName")
-const isDeleting = ref(true)
 
-
-
-
-// declare the props 
 
 const {id,name, amount, percentage,confirmed, hex_code,currency, locale} = defineProps({
   id:String,
@@ -25,25 +18,9 @@ const {id,name, amount, percentage,confirmed, hex_code,currency, locale} = defin
 
 
 })
-function deleteCat(id){
-  // console.log(id)
-  // if(toDeleteCategoryName.value.trim()){
-  //   const catName = toDeleteCategoryName.value
-  //   toDeleteCategoryName.value = catName
-  //   isDeleting.value = true
-    
-    return deleteCategory(id).then((response)=>{
-      console.log(response)
-    }).catch((err)=>{
-      console.log(err,"err")
-    })
- }
- else{
-  toDeleteCategoryName.value = ''
-  isDeleting.value = false
- }
- 
-}
+const toDeleteCategoryName = ref('')
+const isDeleting = ref(true)
+
 
 
 const borderRad = computed(()=>{
@@ -81,10 +58,38 @@ const styleObjectDark = reactive({
   "border-color":changeHSL(hex_code,{s:100,l:80}),
   "border-width" :"2px",
 })
+const deleteButtonStyle = computed(() => ({
+  width: '30px',
+  height: '30px',
+  borderRadius: '50%',
+  backgroundColor: '#ff4d4f',
+  color: 'white',
+  border: 'none',
+  fontWeight: 'bold',
+  cursor: 'pointer',
+}))
+
 
 
   
   const formattedAmount = useCurrencyFormatter(amount,currency,locale)
+  async function deleteCat(category_id) {
+  try {
+    const confirmDelete = confirm('Are you sure you want to delete this category?')
+
+    if (!confirmDelete) return; 
+
+    toDeleteCategoryName.value = name
+    const response = await deleteCategory(category_id)
+    console.log(response, 'Deleted successfully')
+    alert('Category deleted successfully!')
+    
+  } catch (err) {
+    console.error(err)
+    alert('Failed to delete category!')
+  }
+}
+
 
 </script>
 
@@ -100,8 +105,8 @@ const styleObjectDark = reactive({
       </div>
       
 
-      <button v-if="isDeleting" class="deleteCategoryButton" :style="styleObject" @click="deleteCat(id)">
-     -
+      <button v-if="isDeleting" class="deleteCategoryButton" :style="deleteButtonStyle" @click.stop.prevent="deleteCat(id)">
+        ×
       </button>
   </RouterLink>
   
@@ -113,3 +118,6 @@ const styleObjectDark = reactive({
 
 
 </style>
+
+
+
